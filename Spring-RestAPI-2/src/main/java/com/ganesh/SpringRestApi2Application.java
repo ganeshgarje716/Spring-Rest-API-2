48ba332edec501c5d6fe78046feab69297e95b9f
Ganesh <ganeshgarje716@gmail.com>
2026-06-22 18:46:40 +0530

    Create Spring Rest API 2 Project

---
+++ Spring-RestAPI-2/src/main/java/com/ganesh/SpringRestApi2Application.java
@@ -0,0 +1,13 @@
+package com.ganesh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestApi2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestApi2Application.class, args);
+	}
+
+}
