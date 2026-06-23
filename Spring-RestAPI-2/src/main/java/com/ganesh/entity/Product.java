be5827e8d517ef60b09d74c37a0fecc08b555340
Ganesh <ganeshgarje716@gmail.com>
2026-06-23 19:29:25 +0530

    Add Validations on variables

--- Spring-RestAPI-2/src/main/java/com/ganesh/entity/Product.java
+++ Spring-RestAPI-2/src/main/java/com/ganesh/entity/Product.java
@@ -1,5 +1,7 @@
 package com.ganesh.entity;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -8,14 +10,19 @@ import lombok.Data;
 public class Product {
 	
 	
+	@NotNull(message = "Id Is Required")
 	private Integer id;
 	
+	@NotBlank(message = "Id Is Required")
 	private String name;
 	
+	@NotNull(message = "Id Is Required")
 	private Double price;
 	
+	@NotNull(message = "Id Is Required")
 	private Integer qty;
 	
+	@NotBlank(message = "Id Is Required")
 	private String category;
 	
 
